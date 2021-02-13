7d69e8f48e6538ad21298f684c538d7d918167b8
Evan Cope <copev313@gmail.com>
2021-02-13 16:26:15 -0500

    some renaming

--- FirestoreDB.js
+++ FirestoreDB.js
@@ -9,37 +9,26 @@
  * 
  *
  *  Created by: E.Cope                        (Last edit: 2/13/21)
- **********************************************************************
- */
+ *
+ **********************************************************************/
 
 /*** CONSTANTS ***/
 const COLLECTION = 'SCOREBOARD'
 const NAMESLIST = ['Evan', 'Sara', 'McCauley', 'Brandon', 'Dana', 'Kiersten']
+const ALLOWDOCDELETION = false
 const PROPS = PropertiesService.getUserProperties()
 
 // Retreive Stored Properties:
 const [email, key, project_id] = [PROPS.getProperty('client_email'),
                                   PROPS.getProperty('private_key'),
                                   PROPS.getProperty('project_id')]
-
-const doGet = (e) => e      // Performs GET requests.
-const doPost = (e) => e     // Performs POST requests.
+// Performs GET requests.
+const doGet = (e) => e
+// Performs POST requests.
+const doPost = (e) => e
 
 // Create a Firestore object & authenticate:
 const fs = FirestoreApp.getFirestore(email, key, project_id)
-
-
-/**
- * Retreives a document from a collection based on the document's name/id.
- * 
- * @param {string} name -- the document's id value.
- * @param {string} collection (opt) -- the collection's name.
- * @return {object} -- the document object as JSON.
- */
-const getDocumentByName = (name, collection=COLLECTION) => {
-  const req = fs.getDocument(`${collection}/${name}`)
-  return doGet(req)
-}
 
 
 /**
@@ -57,105 +46,91 @@ const _createRoutes = (collection, target_ids=NAMESLIST) => {
   return routes
 }
 
+/*** CONSTANT ***/
+// An array of targetted document routes for team members in NAMESLIST.
+const TEAM = _createRoutes(COLLECTION)  
 
-// CONSTANT {Array}: Targetted document routes.
-const TARGET = _createRoutes(COLLECTION)  
 
+// * * * * * * * DOCUMENT CRUD OPERATIONS * * * * * * * \\
 
 /**
- * Adds a new document to a collection.
+ * Adds a new document to a given collection by specifying the name/id and data to add.
  * 
  * @param {string} doc_name -- the new documents name/id value.
  * @param {object} data (opt) -- key-value data initialized in the document.
  * @param {string} collection (opt) -- the collection to add to.
  */
-const _addDocument = (doc_name,
-                      data={"_totalXP": 0},
-                      collection=COLLECTION) => {
+const _CreateDocument_ = (doc_name,
+                          data={"_totalXP": 0},
+                          collection=COLLECTION) => {
+
+  // Create document request:
   const req = fs.createDocument(`${collection}/${doc_name}`, data)
+  // POST request:
   const res = doPost(req)
+  // Take object returned by POST request & select just the data.
   const parse = JSON.stringify(res.obj)
-  return `Added a new peep to the collection ${collection}!\n${doc_name}: \n${parse}`
+  // Return a confirmation message that the doc has been added to the collection.
+  return `Created a new peep in ${collection}!\n${doc_name}: ${parse}`
 }
 
 
 /**
- * Adds a key-value pair attribute to each document targetted in the given collection.
+ * Retreives a document from a collection based on the document's name/id.
  * 
- * @param {object} data -- the key-value object to add.
- * @param {array} target (opt) -- the document routes to add the data to.
- * @return {string} -- a confirmation message on completion. 
+ * @param {string} doc_name -- the document name/id value to look for.
+ * @param {string} collection (opt) -- the collection's name.
+ * @param {boolean} just_data (opt) -- whether to read in just the data of the doc.
+ * @return {object} -- the document object (or just its data) as JSON.
  */
-const addAttribute = (data, target=TARGET) => {
-  for (let route of target) {
-    doPost(fs.updateDocument(route, data, true))
-  }
-  // Return something to signify the process is complete.
-  return `Added attribute: ${Object.keys(data)[0]} to the XP Score Board!`
+const _ReadDocument_ = (doc_name,
+                        collection=COLLECTION,
+                        just_data=false) => {
+
+  const req = fs.getDocument(`${collection}/${doc_name}`)
+  const res = doGet(req)
+  if (just_data) { return res.obj }
+  else { return res }
 }
 
 
 /**
- * Finds the attribute specified and replaces it with the new key-value data for a specific document in a given collection.
+ * Updates the data of a specific document in a given collection.
  * 
- * @param {object} find --
- * @param {object} replace_with --
- * @param {string} id --
- * @param {string} collection (opt) --
+ * @param {string} doc_name -- the name/id of the document to update.
+ * @param {object} new_data -- the new key-value data used to update the doc.
+ * @param {string} collection (opt) -- the collection where the document can be found.
+ * @param {boolean} only_specific (opt) -- sets whether the update will only affect the specific data from new_data, or will override all the existing data of the document.
  * @return {string} --
  */
-const updateAttribute = (find,
-                         replace_with,
-                         id, collection=COLLECTION) => {
-  
-  //TODO!
+const _UpdateDocument_ = (doc_name,
+                          new_data,
+                          collection=COLLECTION,
+                          only_specific=true) => {
 
+  const req = fs.updateDocument(`${collection}/${doc_name}`,
+                                 new_data,
+                                 only_specific)
+  const res = doPost(req)
+  const json = JSON.stringify(res.obj)
+  return `Document: ${doc_name} has been updated.\nCurrent data: ${json}`
 }
 
 
 /**
- * Deletes a key-value pair attribute from each document targetted in the given collection.
- * 
- * @param {object} attribute -- the key-value pair to remove from each document.
- * @param {array} target (opt) -- an array of routes to each document being targetted.
- * @return {string} -- a confirmation the task has been completed.
- */
-const _deleteAttribute = (attribute, target=TARGET) => {
-  for (let route of target) {
-    let req = fs.getDocument(route)
-    let doc = doGet(req)
-    let data = doc.obj
-    // Grab just the key from the attribute object.
-    const key = Object.keys(attribute)[0]
-    // Delete that specific key-value pair from the data.
-    delete data[key]
-    // Update with the new PATCH
-    doPost(fs.updateDocument(route, data))
-  }
-  return `Attribute ${attribute} has been successfully deleted.`
-}
-
-
-/**
- * Delete a specific document given the document name/id and collection.
+ * Deletes a specific document given the document name/id and collection.
  * With great power comes great responsibility!
  * 
  * @param {string} doc_name -- the name/id of the document to delete.
  * @param {string} collection -- the name of the collection being targetted.
  * @return {string} -- a confirmation upon completion.
  */
-/** (USE WITH CAUTION)
-const __DELETEthisDOCUMENTatYOURownRISK = (doc_name, collection) => {
-  const req = fs.deleteDocument(`${collection}/${doc_name}`)
-  doPost(req)
-  return `THE DOCUMENT <${doc_name}> HAS BEEN DESTROYED.`
-}
-*/
-
-
-// TESTS:
-function netrunner(){
-  const test = ''
-  console.log(email, project_id)
+const _DeleteDocument_ = (doc_name, collection) => {
+  if (ALLOWDOCDELETION) { 
+    doPost(fs.deleteDocument(`${collection}/${doc_name}`))
+    return `THE DOCUMENT <${doc_name}> HAS BEEN DESTROYED.\n\n\n   u monster`
+  } else {
+    return `I'm sorry, Dave. I'm afraid I can't do that. 🔴`
+  }
 }
 
